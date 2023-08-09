08284c93b37847d7f375a613bd54ac28de7b9084
joahJS <91927771+joahJS@users.noreply.github.com>
2023-08-09 17:41:59 +0900

    공지사항 게시판 pager 작업

--- src/scm/ScmNoti.vue
+++ src/scm/ScmNoti.vue
@@ -10,7 +10,7 @@
                     <li>파일</li>
                 </ul>
             </div>
-            <div v-for="item in copyOfData" class="scm-table-body">
+            <div v-for="item in dataPerPage[recentPage]" class="scm-table-body">
                 <ul class="scm-table-line">
                     <li>{{ item.NO }}</li>
                     <li>{{ item.TITLE }}</li>
@@ -24,7 +24,16 @@
                 </ul>
             </div>
         </div>
-        <button type="button" @click="chkChk()">확인용버튼</button>
+        <div class="scm-common-pager">
+            <ul class="scm-pager-list">
+                <li @click="recentPage = recentPage > 1 ? recentPage - 1 : recentPage = 0"><font-awesome-icon icon="fa-solid fa-angle-left" /></li>
+                <li v-for="pageNumber in totalPages" @click="recentPage = pageNumber-1" :class="{ pagerRecentPage: pageNumber == recentPage }">
+                    {{ pageNumber }}
+                </li>
+                <li @click="recentPage = recentPage < totalPageNum - 1 ? recentPage + 1 : recentPage = totalPageNum - 1"><font-awesome-icon icon="fa-solid fa-angle-right" /></li>
+            </ul>
+        </div>
+        <!-- <button type="button" @click="chkPage"> recentPage 확인용 버튼</button> -->
     </div>
 </template>
 
@@ -42,6 +51,37 @@
     function chkChk() {
         console.log(copyOfData)
     }
+
+    const perPage = 5 //페이지 당 글 갯수
+    const totalPageNum = Math.ceil(copyOfData.length / perPage) // 페이지가 총 몇 페이지 나와야하는지 계산
+
+    const totalPages = Array.from({ length: totalPageNum }, (v, i) => i+1); // 모든 페이지 구하기(1페이지~마지막 페이지까지) -> 예시) <  1 2 3 4 5  >
+    console.log(totalPages)
+
+    //각 페이지마다 들어가야할 데이터들 분류하기( pagesDivide() + dataPerPage )
+    function pagesDivide(data = [], size = 1) {
+        const arr = [];
+
+        for (let i = 0; i < data.length; i += size) {
+            arr.push(data.slice(i, i + size));
+        }
+
+        return arr;
+    }
+
+    const dataPerPage = pagesDivide(copyOfData, perPage)
+
+    console.log(dataPerPage)
+
+    const recentPage = ref(0)  // 현재페이지
+
+    function chkPage() {
+        console.log(recentPage.value)
+    }
+
+    watch(recentPage, () => {
+        
+    })
     
 </script>
 
